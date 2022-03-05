b5cf3466d1e14d5bc0b67daba123073bc86fc726
brimuk <be.rightmuk@gmail.com>
2022-03-05 15:21:44 +0300

    Add fast cash form

---
+++ FastCashForm.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FastCashForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.btn5000 = New System.Windows.Forms.Button()
+        Me.btn10000 = New System.Windows.Forms.Button()
+        Me.btn20000 = New System.Windows.Forms.Button()
+        Me.btn30000 = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(19, Byte), Integer), CType(CType(53, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.btnExit)
+        Me.Panel1.Controls.Add(Me.btn30000)
+        Me.Panel1.Controls.Add(Me.btn20000)
+        Me.Panel1.Controls.Add(Me.btn10000)
+        Me.Panel1.Controls.Add(Me.btn5000)
+        Me.Panel1.Controls.Add(Me.lblTitle)
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(803, 451)
+        Me.Panel1.TabIndex = 0
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Verdana", 24.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblTitle.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblTitle.Location = New System.Drawing.Point(293, 44)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(196, 40)
+        Me.lblTitle.TabIndex = 17
+        Me.lblTitle.Text = "Fast Cash"
+        '
+        'btn5000
+        '
+        Me.btn5000.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(19, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.btn5000.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn5000.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn5000.FlatAppearance.BorderColor = System.Drawing.Color.Pink
+        Me.btn5000.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn5000.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn5000.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn5000.Location = New System.Drawing.Point(120, 147)
+        Me.btn5000.Name = "btn5000"
+        Me.btn5000.Size = New System.Drawing.Size(184, 32)
+        Me.btn5000.TabIndex = 18
+        Me.btn5000.Text = "Ksh.5,000"
+        Me.btn5000.UseVisualStyleBackColor = False
+        '
+        'btn10000
+        '
+        Me.btn10000.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(19, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.btn10000.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn10000.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn10000.FlatAppearance.BorderColor = System.Drawing.Color.Pink
+        Me.btn10000.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn10000.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn10000.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn10000.Location = New System.Drawing.Point(432, 147)
+        Me.btn10000.Name = "btn10000"
+        Me.btn10000.Size = New System.Drawing.Size(184, 32)
+        Me.btn10000.TabIndex = 19
+        Me.btn10000.Text = "Ksh.10,000"
+        Me.btn10000.UseVisualStyleBackColor = False
+        '
+        'btn20000
+        '
+        Me.btn20000.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(19, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.btn20000.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn20000.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn20000.FlatAppearance.BorderColor = System.Drawing.Color.Pink
+        Me.btn20000.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn20000.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn20000.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn20000.Location = New System.Drawing.Point(120, 209)
+        Me.btn20000.Name = "btn20000"
+        Me.btn20000.Size = New System.Drawing.Size(184, 32)
+        Me.btn20000.TabIndex = 20
+        Me.btn20000.Text = "Ksh.20,000"
+        Me.btn20000.UseVisualStyleBackColor = False
+        '
+        'btn30000
+        '
+        Me.btn30000.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(19, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.btn30000.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn30000.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn30000.FlatAppearance.BorderColor = System.Drawing.Color.Pink
+        Me.btn30000.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn30000.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn30000.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn30000.Location = New System.Drawing.Point(432, 209)
+        Me.btn30000.Name = "btn30000"
+        Me.btn30000.Size = New System.Drawing.Size(184, 32)
+        Me.btn30000.TabIndex = 21
+        Me.btn30000.Text = "Ksh.30,000"
+        Me.btn30000.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(86, Byte), Integer))
+        Me.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.Pink
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnExit.ForeColor = System.Drawing.Color.White
+        Me.btnExit.Location = New System.Drawing.Point(329, 346)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(89, 30)
+        Me.btnExit.TabIndex = 22
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents btn30000 As Button
+    Friend WithEvents btn20000 As Button
+    Friend WithEvents btn10000 As Button
+    Friend WithEvents btn5000 As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ FastCashForm.vb
@@ -0,0 +1,3 @@
+﻿Public Class FastCashForm
+
+End Class
