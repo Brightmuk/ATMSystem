a6b7e09909acf229758040b571e73901af5c868e
brimuk <be.rightmuk@gmail.com>
2022-03-07 12:20:26 +0300

    Fast cash functionality

--- FastCashForm.Designer.vb
+++ FastCashForm.Designer.vb
@@ -23,12 +23,12 @@ Partial Class FastCashForm
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.lblTitle = New System.Windows.Forms.Label()
-        Me.btn5000 = New System.Windows.Forms.Button()
-        Me.btn10000 = New System.Windows.Forms.Button()
-        Me.btn20000 = New System.Windows.Forms.Button()
-        Me.btn30000 = New System.Windows.Forms.Button()
         Me.btnExit = New System.Windows.Forms.Button()
+        Me.btn30000 = New System.Windows.Forms.Button()
+        Me.btn20000 = New System.Windows.Forms.Button()
+        Me.btn10000 = New System.Windows.Forms.Button()
+        Me.btn5000 = New System.Windows.Forms.Button()
+        Me.lblTitle = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -46,64 +46,19 @@ Partial Class FastCashForm
         Me.Panel1.Size = New System.Drawing.Size(803, 451)
         Me.Panel1.TabIndex = 0
         '
-        'lblTitle
+        'btnExit
         '
-        Me.lblTitle.AutoSize = True
-        Me.lblTitle.Font = New System.Drawing.Font("Verdana", 24.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.lblTitle.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.lblTitle.Location = New System.Drawing.Point(293, 44)
-        Me.lblTitle.Name = "lblTitle"
-        Me.lblTitle.Size = New System.Drawing.Size(196, 40)
-        Me.lblTitle.TabIndex = 17
-        Me.lblTitle.Text = "Fast Cash"
-        '
-        'btn5000
-        '
-        Me.btn5000.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(19, Byte), Integer), CType(CType(60, Byte), Integer))
-        Me.btn5000.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.btn5000.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.btn5000.FlatAppearance.BorderColor = System.Drawing.Color.Pink
-        Me.btn5000.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btn5000.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.btn5000.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.btn5000.Location = New System.Drawing.Point(120, 147)
-        Me.btn5000.Name = "btn5000"
-        Me.btn5000.Size = New System.Drawing.Size(184, 32)
-        Me.btn5000.TabIndex = 18
-        Me.btn5000.Text = "Ksh.5,000"
-        Me.btn5000.UseVisualStyleBackColor = False
-        '
-        'btn10000
-        '
-        Me.btn10000.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(19, Byte), Integer), CType(CType(60, Byte), Integer))
-        Me.btn10000.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.btn10000.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.btn10000.FlatAppearance.BorderColor = System.Drawing.Color.Pink
-        Me.btn10000.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btn10000.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.btn10000.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.btn10000.Location = New System.Drawing.Point(432, 147)
-        Me.btn10000.Name = "btn10000"
-        Me.btn10000.Size = New System.Drawing.Size(184, 32)
-        Me.btn10000.TabIndex = 19
-        Me.btn10000.Text = "Ksh.10,000"
-        Me.btn10000.UseVisualStyleBackColor = False
-        '
-        'btn20000
-        '
-        Me.btn20000.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(19, Byte), Integer), CType(CType(60, Byte), Integer))
-        Me.btn20000.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
-        Me.btn20000.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.btn20000.FlatAppearance.BorderColor = System.Drawing.Color.Pink
-        Me.btn20000.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btn20000.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.btn20000.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
-        Me.btn20000.Location = New System.Drawing.Point(120, 209)
-        Me.btn20000.Name = "btn20000"
-        Me.btn20000.Size = New System.Drawing.Size(184, 32)
-        Me.btn20000.TabIndex = 20
-        Me.btn20000.Text = "Ksh.20,000"
-        Me.btn20000.UseVisualStyleBackColor = False
+        Me.btnExit.BackColor = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(86, Byte), Integer))
+        Me.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.Pink
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnExit.ForeColor = System.Drawing.Color.White
+        Me.btnExit.Location = New System.Drawing.Point(329, 346)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(89, 30)
+        Me.btnExit.TabIndex = 22
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
         '
         'btn30000
         '
@@ -121,27 +76,72 @@ Partial Class FastCashForm
         Me.btn30000.Text = "Ksh.30,000"
         Me.btn30000.UseVisualStyleBackColor = False
         '
-        'btnExit
+        'btn20000
         '
-        Me.btnExit.BackColor = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(86, Byte), Integer))
-        Me.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.Pink
-        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnExit.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.btnExit.ForeColor = System.Drawing.Color.White
-        Me.btnExit.Location = New System.Drawing.Point(329, 346)
-        Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(89, 30)
-        Me.btnExit.TabIndex = 22
-        Me.btnExit.Text = "Exit"
-        Me.btnExit.UseVisualStyleBackColor = False
+        Me.btn20000.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(19, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.btn20000.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn20000.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn20000.FlatAppearance.BorderColor = System.Drawing.Color.Pink
+        Me.btn20000.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn20000.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn20000.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn20000.Location = New System.Drawing.Point(120, 209)
+        Me.btn20000.Name = "btn20000"
+        Me.btn20000.Size = New System.Drawing.Size(184, 32)
+        Me.btn20000.TabIndex = 20
+        Me.btn20000.Text = "Ksh.20,000"
+        Me.btn20000.UseVisualStyleBackColor = False
         '
-        'Form1
+        'btn10000
+        '
+        Me.btn10000.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(19, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.btn10000.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn10000.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn10000.FlatAppearance.BorderColor = System.Drawing.Color.Pink
+        Me.btn10000.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn10000.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn10000.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn10000.Location = New System.Drawing.Point(432, 147)
+        Me.btn10000.Name = "btn10000"
+        Me.btn10000.Size = New System.Drawing.Size(184, 32)
+        Me.btn10000.TabIndex = 19
+        Me.btn10000.Text = "Ksh.10,000"
+        Me.btn10000.UseVisualStyleBackColor = False
+        '
+        'btn5000
+        '
+        Me.btn5000.BackColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(19, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.btn5000.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.btn5000.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn5000.FlatAppearance.BorderColor = System.Drawing.Color.Pink
+        Me.btn5000.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn5000.Font = New System.Drawing.Font("Verdana", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn5000.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn5000.Location = New System.Drawing.Point(120, 147)
+        Me.btn5000.Name = "btn5000"
+        Me.btn5000.Size = New System.Drawing.Size(184, 32)
+        Me.btn5000.TabIndex = 18
+        Me.btn5000.Text = "Ksh.5,000"
+        Me.btn5000.UseVisualStyleBackColor = False
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Verdana", 24.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblTitle.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblTitle.Location = New System.Drawing.Point(293, 44)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(196, 40)
+        Me.lblTitle.TabIndex = 17
+        Me.lblTitle.Text = "Fast Cash"
+        '
+        'FastCashForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.Panel1)
-        Me.Name = "Form1"
+        Me.Name = "FastCashForm"
         Me.Text = "Form1"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
--- FastCashForm.vb
+++ FastCashForm.vb
@@ -1,3 +1,86 @@
-﻿Public Class FastCashForm
+﻿Imports MySql.Data.MySqlClient
 
+Public Class FastCashForm
+    Dim CurrentBalance As Integer
+    Public Sub Withdraw(amount As Int32)
+        Try
+            LandingPageForm.conn.Open()
+            Dim query1 = "UPDATE bankAccounts SET Balance=Balance-'" & (amount + 20) & "' WHERE owner='" & LandingPageForm.IdNumber & "';"
+            Dim cmd1 = New MySqlCommand(query1, LandingPageForm.conn)
+            cmd1.ExecuteNonQuery()
+
+            Dim transcId = CLng(DateTime.UtcNow.Subtract(New DateTime(1970, 1, 1)).TotalMilliseconds)
+            Dim query2 = "INSERT INTO transactions(TransactionId, Owner, Type, Amount) VALUES ('" & transcId & "','" & LandingPageForm.IdNumber & "', 'Withdraw','" & amount & "');"
+            Dim cmd2 = New MySqlCommand(query2, LandingPageForm.conn)
+            cmd2.ExecuteNonQuery()
+
+            MessageBox.Show("Withdrawal was successful! Transaction charge ksh.20")
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+        LandingPageForm.conn.Close()
+
+    End Sub
+    Public Sub GetBalance()
+        Try
+            LandingPageForm.conn.Open()
+            Dim query = "SELECT Balance FROM bankAccounts WHERE owner='" & LandingPageForm.IdNumber & "';"
+            Dim cmd = New MySqlCommand(query, LandingPageForm.conn)
+            Dim balance = Convert.ToString(cmd.ExecuteScalar)
+
+            CurrentBalance = cmd.ExecuteScalar
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+        LandingPageForm.conn.Close()
+
+    End Sub
+    Private Sub FastCash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GetBalance()
+
+    End Sub
+
+    Private Sub btn5000_Click(sender As Object, e As EventArgs) Handles btn5000.Click
+        If 5000 + 120 > CurrentBalance Then
+            MessageBox.Show("Insufficient balance to withdraw ksh." & 5000 & "! You need a minimum balance of ksh." & 5000 + 120 & " to  withdraw ksh." & 5000 & "")
+        Else
+            Withdraw(5000)
+
+        End If
+
+    End Sub
+
+    Private Sub btn10000_Click(sender As Object, e As EventArgs) Handles btn10000.Click
+
+        If 10000 + 120 > CurrentBalance Then
+            MessageBox.Show("Insufficient balance to withdraw ksh." & 20000 & "! You need a minimum balance of ksh." & 20000 + 120 & " to  withdraw ksh." & 20000 & "")
+        Else
+            Withdraw(10000)
+
+        End If
+    End Sub
+
+    Private Sub btn20000_Click(sender As Object, e As EventArgs) Handles btn20000.Click
+        If 20000 + 120 > CurrentBalance Then
+            MessageBox.Show("Insufficient balance to withdraw ksh." & 20000 & "! You need a minimum balance of ksh." & 20000 + 120 & " to  withdraw ksh." & 20000 & "")
+        Else
+            Withdraw(10000)
+
+        End If
+    End Sub
+
+    Private Sub btn30000_Click(sender As Object, e As EventArgs) Handles btn30000.Click
+        If 30000 + 120 > CurrentBalance Then
+            MessageBox.Show("Insufficient balance to withdraw ksh." & 30000 & "! You need a minimum balance of ksh." & 30000 + 120 & " to  withdraw ksh." & 30000 & "")
+        Else
+            Withdraw(10000)
+
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
 End Class
